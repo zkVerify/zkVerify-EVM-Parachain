79a06de7645b97e152963db80b2f562b975a7284
Daniele Rogora <danielerogora@horizenlabs.io>
2025-07-04 14:34:48 +0200

    Enable treasury payments and add tests

--- runtime/src/tests/constants_test.rs
+++ runtime/src/tests/constants_test.rs
@@ -209,6 +209,10 @@ mod runtime_tests {
 mod xcm_tests {
     use super::*;
     use configs::xcm::*;
+    use hex_literal::hex;
+    use xcm::latest::prelude::*;
+    use xcm::opaque::latest::Junctions::X1;
+    use xcm_executor::traits::ConvertLocation;
 
     #[test]
     fn xcm_executor_constants() {
@@ -223,6 +227,20 @@ mod xcm_tests {
         assert_eq!(
             <Runtime as pallet_xcm::Config>::VERSION_DISCOVERY_QUEUE_SIZE,
             100
+        );
+    }
+
+    #[test]
+    fn relay_treasury_account() {
+        let loc = Location {
+            parents: 1,
+            interior: X1([PalletInstance(14)].into()), // TODO: replace with the value taken
+                                                       // directly from zkv_runtime?
+        };
+        assert_eq!(
+            LocationAccountId32ToAccountId::convert_location(&loc)
+                .expect("Location is not convertible; qed"),
+            hex!("f260e064445be1b5e12e5cef1e1c11fc4c4b4963").into(),
         );
     }
 }
