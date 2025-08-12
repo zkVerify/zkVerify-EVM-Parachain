182a9e5fb520f2fd031f4fab8903e7c906aee49a
Daniele Rogora <danielerogora@horizenlabs.io>
2025-08-12 12:45:06 +0200

    Bump node and runtime versions 0.2.2-1.0.0

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -173,7 +173,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: Cow::Borrowed("tvflow-runtime"),
     impl_name: Cow::Borrowed("vflow-node"),
     authoring_version: 1,
-    spec_version: 2000,
+    spec_version: 1_000_000,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
